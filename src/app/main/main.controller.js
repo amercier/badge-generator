e1b870e0a4fb0204b6fd8604d69673349ac5c9ad
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-02-11 19:00:11 -0800

    Fixed broken style

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -14,8 +14,7 @@ class MainCtrl {
     BadgeService.parseTemplate = function(template) {
       return template
         .replace('{repository}', $scope.data.repository)
-        .replace('{branch}', $scope.data.branch)
-        .replace('{style}', $scope.data.style);
+        .replace('{branch}', $scope.data.branch);
     };
 
     BadgeService.parseUrl = function(url, noCache) {
@@ -30,97 +29,97 @@ class MainCtrl {
       return BadgeService.parseUrl(this.url, noCache);
     };
 
-    BadgeService.prototype.getImageUrl = function(noCache) {
-      return BadgeService.parseUrl(this.imageUrl, noCache);
+    BadgeService.prototype.getImageUrl = function(style, noCache) {
+      return BadgeService.parseUrl(this.imageUrl + (style === 'default' ? '' : '?style=' + style), noCache);
     };
 
-    BadgeService.prototype.getMarkDown = function() {
-      return '[![' + this.title + '](' + this.getImageUrl() + ')](' + this.getUrl() + ')';
+    BadgeService.prototype.getMarkDown = function(style) {
+      return '[![' + this.title + '](' + this.getImageUrl(style) + ')](' + this.getUrl() + ')';
     };
 
     $scope.services = [
       new BadgeService({
         name: 'Travis CI',
         url: 'https://travis-ci.org/{repository}',
-        imageUrl: 'https://img.shields.io/travis/{repository}/{branch}.svg?style={style}',
+        imageUrl: 'https://img.shields.io/travis/{repository}/{branch}.svg',
         title: 'Build Status'
       }),
       new BadgeService({
         name: 'CodeShip',
         url: 'https://codeship.com/projects/{repository}',
-        imageUrl: 'https://img.shields.io/codeship/{repository}/{branch}.svg?style={style}',
+        imageUrl: 'https://img.shields.io/codeship/{repository}/{branch}.svg',
         title: 'Build Status'
       }),
       new BadgeService({
         name: 'Code Climate',
         url: 'https://codeclimate.com/github/{repository}',
-        imageUrl: 'https://img.shields.io/codeclimate/github/{repository}.svg?style={style}',
+        imageUrl: 'https://img.shields.io/codeclimate/github/{repository}.svg',
         title: 'Code Climate'
       }),
       new BadgeService({
         name: 'Code Climate (code coverage)',
         url: 'https://codeclimate.com/github/{repository}',
-        imageUrl: 'https://img.shields.io/codeclimate/coverage/github/{repository}.svg?style={style}',
+        imageUrl: 'https://img.shields.io/codeclimate/coverage/github/{repository}.svg',
         title: 'Test Coverage'
       }),
       new BadgeService({
         name: 'Coveralls',
         url: 'https://coveralls.io/r/{repository}?branch={branch}',
-        imageUrl: 'http://img.shields.io/coveralls/{repository}/{branch}.svg?style={style}',
+        imageUrl: 'http://img.shields.io/coveralls/{repository}/{branch}.svg',
         title: 'Test Coverage'
       }),
       new BadgeService({
         name: 'Gemnasium',
         url: 'https://gemnasium.com/{repository}',
-        imageUrl: 'http://img.shields.io/gemnasium/{repository}.svg?style={style}',
+        imageUrl: 'http://img.shields.io/gemnasium/{repository}.svg',
         title: 'Dependency Status'
       }),
       new BadgeService({
         name: 'VersionEye',
         url: 'https://www.versioneye.com/user/projects/{repository}',
-        imageUrl: 'https://www.versioneye.com/user/projects/{repository}/badge.svg?style={style}',
+        imageUrl: 'https://www.versioneye.com/user/projects/{repository}/badge.svg',
         title: 'Dependency Status'
       }),
       new BadgeService({
         name: 'David DM',
         url: 'https://david-dm.org/{repository}',
-        imageUrl: 'https://img.shields.io/david/{repository}.svg?style={style}',
+        imageUrl: 'https://img.shields.io/david/{repository}.svg',
         title: 'Dependency Status'
       }),
       new BadgeService({
         name: 'David DM (dev)',
         url: 'https://david-dm.org/{repository}#info=devDependencies',
-        imageUrl: 'https://img.shields.io/david/dev/{repository}.svg?style={style}',
+        imageUrl: 'https://img.shields.io/david/dev/{repository}.svg',
         title: 'devDependency Status'
       }),
       new BadgeService({
         name: 'David DM (peer)',
         url: 'https://david-dm.org/{repository}#info=devDependencies',
-        imageUrl: 'https://img.shields.io/david/peer/{repository}.svg?style={style}',
+        imageUrl: 'https://img.shields.io/david/peer/{repository}.svg',
         title: 'peerDependency Status'
       }),
       new BadgeService({
         name: 'Packagist',
         url: 'https://gemnasium.com/{repository}',
-        imageUrl: 'https://img.shields.io/packagist/v/{repository}.svg?style={style}',
+        imageUrl: 'https://img.shields.io/packagist/v/{repository}.svg',
         title: 'Latest Stable Version'
       }),
       new BadgeService({
         name: 'NPM',
         url: 'https://www.npmjs.com/package/{repository}',
-        imageUrl: 'https://img.shields.io/npm/v/{repository}.svg?style={style}',
+        imageUrl: 'https://img.shields.io/npm/v/{repository}.svg',
         title: 'Latest Stable Version'
       }),
       new BadgeService({
         name: 'NPM Downloads',
         url: 'https://www.npmjs.com/package/{repository}',
-        imageUrl: 'https://img.shields.io/npm/dm/{repository}.svg?style={style}',
+        imageUrl: 'https://img.shields.io/npm/dm/{repository}.svg',
         title: 'NPM Downloads'
       }),
       new BadgeService({
         name: 'Bower',
         url: 'http://bower.io/search/?q={repository}',
-        imageUrl: 'https://img.shields.io/bower/v/{repository}.svg?style={style}',
+        imageUrl: 'https://img.shields.io/bower/v/{repository}.svg',
         title: 'Latest Stable Version'
       })
     ];
@@ -128,7 +127,7 @@ class MainCtrl {
     $scope.data = {
       repository: '',
       branch: 'master',
-      style: 'flat-square',
+      style: 'default',
       services: $scope.services.map(function(service) {
         return {
           service: service,
@@ -145,7 +144,7 @@ class MainCtrl {
 
     $scope.getMarkDown = function() {
       return $scope.getSelectedServices()
-        .map(function(s) { return s.getMarkDown(); })
+        .map(function(s) { return s.getMarkDown($scope.data.style); })
         .join('\n') + '\n\n';
     };
   }
