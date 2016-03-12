203974bdcf82482e62d28d2544ac432dabcb4c9c
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-03-11 17:23:44 -0800

    Extracted BadgeService and config from Main controller

---
+++ src/app/main/badge-service.js
@@ -0,0 +1,35 @@
+export default class BadgeService {
+  constructor(config, data) {
+    this.name = config.name;
+    this.url = config.url;
+    this.imageUrl = config.imageUrl;
+    this.title = config.title;
+    this.data = data;
+  }
+
+  static parseTemplate(template, { repository, branch }) {
+    return template
+      .replace('{repository}', repository)
+      .replace('{branch}', branch);
+  }
+
+  static parseUrl(url, data, noCache) {
+    var parsed = BadgeService.parseTemplate(url, data);
+    if (noCache) {
+      parsed += (url.indexOf('?') === -1 ? '?' : '&') + Math.floor(new Date().getTime() / 1000);
+    }
+    return parsed;
+  }
+
+  getUrl(noCache) {
+    return BadgeService.parseUrl(this.url, this.data, noCache);
+  }
+
+  getImageUrl(style, noCache) {
+    return BadgeService.parseUrl(this.imageUrl + (style === 'default' ? '' : '?style=' + style), this.data, noCache);
+  }
+
+  getMarkDown(style) {
+    return '[![' + this.title + '](' + this.getImageUrl(style) + ')](' + this.getUrl() + ')';
+  }
+}
---
+++ src/app/main/config.js
@@ -0,0 +1,134 @@
+export default [
+  {
+    name: 'GitHub forks',
+    url: 'https://github.com/{repository}',
+    imageUrl: 'https://img.shields.io/github/forks/{repository}.svg',
+    title: 'Build Status'
+  },
+  {
+    name: 'GitHub stars',
+    url: 'https://github.com/{repository}',
+    imageUrl: 'https://img.shields.io/github/stars/{repository}.svg',
+    title: 'Build Status'
+  },
+  {
+    name: 'Travis CI',
+    url: 'https://travis-ci.org/{repository}',
+    imageUrl: 'https://img.shields.io/travis/{repository}/{branch}.svg',
+    title: 'Build Status'
+  },
+  {
+    name: 'CircleCI',
+    url: 'https://circleci.com/gh/{repository}',
+    imageUrl: 'https://img.shields.io/circleci/project/{repository}/{branch}.svg',
+    title: 'Build Status'
+  },
+  {
+    name: 'CodeShip',
+    url: 'https://codeship.com/projects/{repository}',
+    imageUrl: 'https://circleci.com/gh/{repository}.svg?style=svg',
+    title: 'Build Status'
+  },
+  {
+    name: 'Code Climate',
+    url: 'https://codeclimate.com/github/{repository}',
+    imageUrl: 'https://img.shields.io/codeclimate/github/{repository}.svg',
+    title: 'Code Climate'
+  },
+  {
+    name: 'Code Climate (code coverage)',
+    url: 'https://codeclimate.com/github/{repository}',
+    imageUrl: 'https://img.shields.io/codeclimate/coverage/github/{repository}.svg',
+    title: 'Test Coverage'
+  },
+  {
+    name: 'Coveralls',
+    url: 'https://coveralls.io/r/{repository}?branch={branch}',
+    imageUrl: 'http://img.shields.io/coveralls/{repository}/{branch}.svg',
+    title: 'Test Coverage'
+  },
+  {
+    name: 'Codecov',
+    url: 'https://codecov.io/github/{repository}?branch={branch}',
+    imageUrl: 'https://img.shields.io/codecov/c/github/{repository}/{branch}.svg',
+    title: 'Test Coverage'
+  },
+  {
+    name: 'Gemnasium',
+    url: 'https://gemnasium.com/{repository}',
+    imageUrl: 'http://img.shields.io/gemnasium/{repository}.svg',
+    title: 'Dependency Status'
+  },
+  {
+    name: 'VersionEye',
+    url: 'https://www.versioneye.com/user/projects/{repository}',
+    imageUrl: 'https://www.versioneye.com/user/projects/{repository}/badge.svg',
+    title: 'Dependency Status'
+  },
+  {
+    name: 'David DM',
+    url: 'https://david-dm.org/{repository}',
+    imageUrl: 'https://img.shields.io/david/{repository}.svg',
+    title: 'Dependency Status'
+  },
+  {
+    name: 'David DM (dev)',
+    url: 'https://david-dm.org/{repository}#info=devDependencies',
+    imageUrl: 'https://img.shields.io/david/dev/{repository}.svg',
+    title: 'devDependency Status'
+  },
+  {
+    name: 'David DM (peer)',
+    url: 'https://david-dm.org/{repository}#info=devDependencies',
+    imageUrl: 'https://img.shields.io/david/peer/{repository}.svg',
+    title: 'peerDependency Status'
+  },
+  {
+    name: 'Packagist',
+    url: 'https://gemnasium.com/{repository}',
+    imageUrl: 'https://img.shields.io/packagist/v/{repository}.svg',
+    title: 'Latest Stable Version'
+  },
+  {
+    name: 'NPM',
+    url: 'https://www.npmjs.com/package/{repository}',
+    imageUrl: 'https://img.shields.io/npm/v/{repository}.svg',
+    title: 'Latest Stable Version'
+  },
+  {
+    name: 'NPM License',
+    url: 'https://www.npmjs.com/package/{repository}',
+    imageUrl: 'https://img.shields.io/npm/l/{repository}.svg',
+    title: 'License'
+  },
+  {
+    name: 'NPM Total Downloads',
+    url: 'https://www.npmjs.com/package/{repository}',
+    imageUrl: 'https://img.shields.io/npm/dt/{repository}.svg',
+    title: 'NPM Downloads'
+  },
+  {
+    name: 'NPM Downloads per month',
+    url: 'https://www.npmjs.com/package/{repository}',
+    imageUrl: 'https://img.shields.io/npm/dm/{repository}.svg',
+    title: 'NPM Downloads'
+  },
+  {
+    name: 'Bower',
+    url: 'http://bower.io/search/?q={repository}',
+    imageUrl: 'https://img.shields.io/bower/v/{repository}.svg',
+    title: 'Latest Stable Version'
+  },
+  {
+    name: 'ESDoc',
+    url: 'https://doc.esdoc.org/github.com/{repository}/',
+    imageUrl: 'https://doc.esdoc.org/github.com/{repository}/badge.svg',
+    title: 'API Documentation'
+  },
+  {
+    name: 'Melpa',
+    url: 'http://melpa.org/#/{repository}/',
+    imageUrl: 'http://melpa.org/packages/{repository}-badge.svg',
+    title: 'MELPA'
+  }
+];
