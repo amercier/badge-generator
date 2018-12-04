34f0c25b02c8ccaa12798dd6c1efe32d9fdf0245
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-04 14:20:06 +0100

    Enabled Flow in existing code base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+// @flow
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component<{}> {
   render() {
     return (
       <div className="App">
