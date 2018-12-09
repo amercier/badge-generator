aa55fad52ba3f115ffef3bb36be01be7e00f1653
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-09 20:02:44 +0100

    Ported existing features to React

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,273 @@
 // @flow
 
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { withStyles } from '@material-ui/core/styles';
+import {
+  type Service,
+  services as badgeServices,
+  styles as badgeStyles,
+  formats as badgeFormats,
+} from './config';
+
+type AppProps = {|
+  classes: {
+    container: string,
+    paper: string,
+    gridItem: string,
+    subtitle: string,
+    badges: string,
+    badge: string,
+    code: string,
+  },
+|};
+
+type AppState = {|
+  repository: string,
+  style: string,
+  services: any[],
+  formatIndex: number,
+|};
+
+type InputEvent = SyntheticInputEvent<HTMLInputElement>;
+
+function styleName(style: string) {
+  const name = style.replace(/-/g, ' ');
+  return name.charAt(0).toUpperCase() + name.slice(1);
 }
+
+const styles = ({ spacing, typography }) => ({
+  container: {
+    width: '100%',
+    maxWidth: 80 * typography.fontSize,
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    marginTop: 4 * spacing.unit,
+    marginBottom: 4 * spacing.unit,
+    paddingRight: 2 * spacing.unit,
+    paddingLeft: 2 * spacing.unit,
+  },
+  paper: {
+    marginTop: 4 * spacing.unit,
+    paddingTop: '1px', // prevents magin collapse
+    paddingBottom: 4 * spacing.unit,
+    paddingRight: 4 * spacing.unit,
+    paddingLeft: 4 * spacing.unit,
+  },
+  gridItem: {
+    marginTop: 2 * spacing.unit,
+    marginBottom: 2 * spacing.unit,
+  },
+  subtitle: {
+    marginTop: 2 * spacing.unit,
+  },
+  badges: {
+    lineHeight: '20px',
+  },
+  badge: {
+    marginRight: spacing.unit / 2,
+  },
+  code: {
+    fontFamily: 'Roboto Mono, monospace',
+    fontSize: typography.fontSize - 3,
+    lineHeight: 1.8,
+    overflowX: 'auto',
+    paddingBottom: spacing.unit,
+  },
+});
+
+class App extends Component<AppProps, AppState> {
+  state = {
+    repository: '',
+    style: badgeStyles[0],
+    services: badgeServices.map(badgeService => !!badgeService.enabled),
+    formatIndex: badgeFormats.findIndex(badgeFormat => badgeFormat.default),
+  };
+
+  get enabledServices(): Service[] {
+    const { services } = this.state;
+    return services.reduce(
+      (acc: Service[], enabled: boolean, index: number) => {
+        if (enabled) {
+          acc.push(this.formatService(badgeServices[index]));
+        }
+        return acc;
+      },
+      [],
+    );
+  }
+
+  handleRepositoryChange = (event: InputEvent) => {
+    this.setState({ repository: event.target.value });
+  };
+
+  handleServiceToggle = (index: number) => (event: InputEvent) => {
+    const { services } = this.state;
+    const updatedServices = [...services];
+    updatedServices[index] = event.target.checked;
+    this.setState({ services: updatedServices });
+  };
+
+  handleStyleChange = (event: InputEvent, style: string) => {
+    this.setState({ style });
+  };
+
+  formatUrl(url: string) {
+    const { repository } = this.state;
+    return url
+      .replace('{repository}', repository)
+      .replace('{branch}', 'master');
+  }
+
+  formatService({ name, title, url, imageUrl }: Service): Service {
+    const { style } = this.state;
+    return {
+      name,
+      title,
+      url: this.formatUrl(url),
+      imageUrl: `${this.formatUrl(imageUrl)}?style=${style}`,
+    };
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { repository, services, style, formatIndex } = this.state;
+    const { enabledServices } = this;
+    const format = badgeFormats[formatIndex];
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <CssBaseline />
+        <Typography variant="h4" headlineMapping={{ h4: 'h1' }}>
+          Github Badge Generator
+        </Typography>
+        <Paper className={classes.paper}>
+          <Grid container>
+            <Grid item xs={12} className={classes.gridItem}>
+              <TextField
+                label="Repository"
+                helperText='Ex: "lodash" (npm name), "facebook/react" (Github slug), etc.'
+                margin="normal"
+                fullWidth
+                value={repository}
+                onChange={this.handleRepositoryChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={10} className={classes.gridItem}>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">Services</FormLabel>
+                <Grid container>
+                  {badgeServices.map(({ name }, index) => (
+                    <Grid
+                      key={`service-${name.replace(/ /g, '-').toLowerCase()}`}
+                      item
+                      xs={12}
+                      sm={6}
+                      md={4}
+                      lg={3}
+                    >
+                      <FormControlLabel
+                        label={name}
+                        title={name}
+                        control={
+                          <Checkbox
+                            checked={services[index]}
+                            onChange={this.handleServiceToggle(index)}
+                            value={`${index}`}
+                          />
+                        }
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={2} className={classes.gridItem}>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">Style</FormLabel>
+                <RadioGroup
+                  aria-label="Style"
+                  name="style"
+                  value={style}
+                  onChange={this.handleStyleChange}
+                >
+                  {badgeStyles.map(badgeStyle => (
+                    <FormControlLabel
+                      key={`style-${badgeStyle}`}
+                      value={badgeStyle}
+                      control={<Radio />}
+                      label={styleName(badgeStyle)}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Paper>
+        {repository && enabledServices.length > 0 && (
+          <Paper className={classes.paper}>
+            <Typography
+              variant="h6"
+              className={classes.subtitle}
+              headlineMapping={{ h6: 'h2' }}
+            >
+              Preview
+            </Typography>
+            <p className={classes.badges}>
+              {enabledServices.map(({ title, url, imageUrl }) => (
+                <a
+                  key={`image-${title.replace(/ /g, '-').toLowerCase()}`}
+                  href={url
+                    .replace('{repository}', repository)
+                    .replace('{branch}', 'master')}
+                  className={classes.badge}
+                  title={title}
+                >
+                  <img
+                    src={imageUrl
+                      .replace('{repository}', repository)
+                      .replace('{branch}', 'master')}
+                    alt={title}
+                  />
+                </a>
+              ))}
+            </p>
+            <Typography
+              variant="h6"
+              className={classes.subtitle}
+              headlineMapping={{ h6: 'h2' }}
+            >
+              Source code
+            </Typography>
+            <pre className={classes.code}>
+              {enabledServices.map(
+                ({ title, url, imageUrl }) =>
+                  `${format.template(
+                    title,
+                    url
+                      .replace('{repository}', repository)
+                      .replace('{branch}', 'master'),
+                    imageUrl
+                      .replace('{repository}', repository)
+                      .replace('{branch}', 'master'),
+                  )}\n`,
+              )}
+            </pre>
+          </Paper>
+        )}
+      </form>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
---
+++ src/config.js
@@ -0,0 +1,242 @@
+// @flow
+
+export type Service = {|
+  name: string,
+  title: string,
+  url: string,
+  imageUrl: string,
+  enabled?: boolean,
+|};
+
+export type Format = {|
+  name: string,
+  template: (title: string, url: string, img: string) => string,
+  default?: boolean,
+|};
+
+export const services: Service[] = [
+  {
+    name: 'GitHub forks',
+    url: 'https://github.com/{repository}',
+    imageUrl: 'https://img.shields.io/github/forks/{repository}.svg',
+    title: 'Build Status',
+  },
+  {
+    name: 'GitHub stars',
+    url: 'https://github.com/{repository}',
+    imageUrl: 'https://img.shields.io/github/stars/{repository}.svg',
+    title: 'Build Status',
+  },
+  {
+    name: 'GitHub license',
+    url: 'https://github.com/{repository}',
+    imageUrl: 'https://img.shields.io/github/license/{repository}.svg',
+    title: 'License',
+  },
+  {
+    name: 'Travis CI',
+    url: 'https://travis-ci.org/{repository}',
+    imageUrl: 'https://img.shields.io/travis/{repository}/{branch}.svg',
+    title: 'Build Status',
+  },
+  {
+    name: 'CircleCI',
+    url: 'https://circleci.com/gh/{repository}',
+    imageUrl:
+      'https://img.shields.io/circleci/project/{repository}/{branch}.svg',
+    title: 'Build Status',
+  },
+  {
+    name: 'CodeShip',
+    url: 'https://codeship.com/projects/{repository}',
+    imageUrl: 'https://circleci.com/gh/{repository}.svg?style=svg',
+    title: 'Build Status',
+  },
+  {
+    name: 'Code Climate (maintainability)',
+    url: 'https://codeclimate.com/github/{repository}',
+    imageUrl:
+      'https://img.shields.io/codeclimate/maintainability/{repository}.svg',
+    title: 'Maintainability',
+  },
+  {
+    name: 'Code Climate (% maintainability)',
+    url: 'https://codeclimate.com/github/{repository}',
+    imageUrl:
+      'https://img.shields.io/codeclimate/maintainability-percentage/{repository}.svg',
+    title: 'Maintainability',
+  },
+  {
+    name: 'Code Climate (tech debt)',
+    url: 'https://codeclimate.com/github/{repository}',
+    imageUrl: 'https://img.shields.io/codeclimate/tech-debt/{repository}.svg',
+    title: 'Technical Debt',
+  },
+  {
+    name: 'Code Climate (code coverage)',
+    url: 'https://codeclimate.com/github/{repository}',
+    imageUrl:
+      'https://img.shields.io/codeclimate/coverage/github/{repository}.svg',
+    title: 'Test Coverage',
+  },
+  {
+    name: 'Coveralls',
+    url: 'https://coveralls.io/r/{repository}?branch={branch}',
+    imageUrl: 'http://img.shields.io/coveralls/{repository}/{branch}.svg',
+    title: 'Test Coverage',
+  },
+  {
+    name: 'Codecov',
+    url: 'https://codecov.io/github/{repository}?branch={branch}',
+    imageUrl:
+      'https://img.shields.io/codecov/c/github/{repository}/{branch}.svg',
+    title: 'Test Coverage',
+  },
+  {
+    name: 'Gemnasium',
+    url: 'https://gemnasium.com/{repository}',
+    imageUrl: 'http://img.shields.io/gemnasium/{repository}.svg',
+    title: 'Dependency Status',
+  },
+  {
+    name: 'VersionEye',
+    url: 'https://www.versioneye.com/user/projects/{repository}',
+    imageUrl: 'https://www.versioneye.com/user/projects/{repository}/badge.svg',
+    title: 'Dependency Status',
+  },
+  {
+    name: 'David DM',
+    url: 'https://david-dm.org/{repository}',
+    imageUrl: 'https://img.shields.io/david/{repository}.svg',
+    title: 'Dependency Status',
+  },
+  {
+    name: 'David DM (dev)',
+    url: 'https://david-dm.org/{repository}#info=devDependencies',
+    imageUrl: 'https://img.shields.io/david/dev/{repository}.svg',
+    title: 'devDependency Status',
+  },
+  {
+    name: 'David DM (peer)',
+    url: 'https://david-dm.org/{repository}#info=devDependencies',
+    imageUrl: 'https://img.shields.io/david/peer/{repository}.svg',
+    title: 'peerDependency Status',
+  },
+  {
+    name: 'Packagist',
+    url: 'https://packagist.org/packages/{repository}',
+    imageUrl: 'https://img.shields.io/packagist/v/{repository}.svg',
+    title: 'Latest Stable Version',
+  },
+  {
+    name: 'Packagist PHP version',
+    url: 'https://packagist.org/packages/{repository}',
+    imageUrl: 'https://img.shields.io/packagist/php-v/{repository}.svg',
+    title: 'PHP version',
+  },
+  {
+    name: 'Packagist License',
+    url: 'https://packagist.org/packages/{repository}',
+    imageUrl: 'https://img.shields.io/packagist/l/{repository}.svg',
+    title: 'License',
+  },
+  {
+    name: 'NPM',
+    url: 'https://www.npmjs.com/package/{repository}',
+    imageUrl: 'https://img.shields.io/npm/v/{repository}.svg',
+    title: 'Latest Stable Version',
+  },
+  {
+    name: 'NPM License',
+    url: 'https://www.npmjs.com/package/{repository}',
+    imageUrl: 'https://img.shields.io/npm/l/{repository}.svg',
+    title: 'License',
+  },
+  {
+    name: 'NPM Total Downloads',
+    url: 'https://www.npmjs.com/package/{repository}',
+    imageUrl: 'https://img.shields.io/npm/dt/{repository}.svg',
+    title: 'NPM Downloads',
+  },
+  {
+    name: 'NPM Downloads per month',
+    url: 'https://www.npmjs.com/package/{repository}',
+    imageUrl: 'https://img.shields.io/npm/dm/{repository}.svg',
+    title: 'NPM Downloads',
+  },
+  {
+    name: 'Bower',
+    url: 'http://bower.io/search/?q={repository}',
+    imageUrl: 'https://img.shields.io/bower/v/{repository}.svg',
+    title: 'Latest Stable Version',
+  },
+  {
+    name: 'ESDoc',
+    url: 'https://doc.esdoc.org/github.com/{repository}/',
+    imageUrl: 'https://doc.esdoc.org/github.com/{repository}/badge.svg',
+    title: 'API Documentation',
+  },
+  {
+    name: 'Greenkeeper',
+    url: 'https://github.com/{repository}/issues?q=label%3Agreenkeeper',
+    imageUrl: 'https://badges.greenkeeper.io/{repository}.svg',
+    title: 'Greenkeeper',
+  },
+  {
+    name: 'Melpa',
+    url: 'http://melpa.org/#/{repository}/',
+    imageUrl: 'http://melpa.org/packages/{repository}-badge.svg',
+    title: 'MELPA',
+  },
+];
+
+export const formats: Format[] = [
+  {
+    name: 'URL',
+    template: (title, url) => `${url}`,
+  },
+  {
+    name: 'Image URL',
+    template: (title, url, img) => `${img}`,
+  },
+  {
+    name: 'Markdown',
+    template: (title, url, img) => `[![${title}](${img})](${url})`,
+    default: true,
+  },
+  {
+    name: 'Org Mode',
+    template: (title, url, img) => `[[${url}][file:${img}]]`,
+  },
+  {
+    name: 'Textile',
+    template: (title, url, img) => `!${img}!:${url}`,
+  },
+  {
+    name: 'Rdoc',
+    template: (title, url, img) =>
+      `{<img src="${img}" alt="${title}" />}[${url}]`,
+  },
+  {
+    name: 'AsciiDoc',
+    template: (title, url, img) =>
+      `image:${img}["Build Status", link="${url}"]`,
+  },
+  {
+    name: 'RST',
+    template: (title, url, img) => `.. image:: ${img}\n  :target: ${url}`,
+  },
+  {
+    name: 'Pod',
+    template: (title, url, img) =>
+      `=for HTML <a href="${url}"><img src="${img}"></a>`,
+  },
+];
+
+export const styles = [
+  'default',
+  'plastic',
+  'flat-square',
+  'for-the-badge',
+  'social',
+];
