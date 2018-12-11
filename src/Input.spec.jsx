ff36ac83b81c860c2bac63f685364997883b3be6
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-11 11:53:04 +0100

    Added more specs for Input module

--- src/Input.spec.jsx
+++ src/Input.spec.jsx
@@ -2,8 +2,12 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Input from './Input';
-import { services, styles } from './config';
+import { type Service, services, styles } from './config';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 const noop = () => {};
 const serviceSelection = services.map((service, index) => !!(index % 2));
@@ -21,5 +25,66 @@ describe('Input', () => {
       />,
     );
     expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('calls handleRepositoryChange when respository changes', () => {
+    const handleRepositoryChangeMock = jest.fn();
+    const input = mount(
+      <Input
+        repository="REPOSITORY"
+        style={styles[0]}
+        serviceSelection={serviceSelection}
+        handleRepositoryChange={handleRepositoryChangeMock}
+        handleServiceToggle={noop}
+        handleStyleChange={noop}
+      />,
+    );
+
+    expect(handleRepositoryChangeMock).toHaveBeenCalledTimes(0);
+    input.find('input[type="text"]').simulate('change');
+    expect(handleRepositoryChangeMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls handleServiceToggle when a service is toggled', () => {
+    const handleServiceToggleMocks = services.map(() => jest.fn());
+    const input = mount(
+      <Input
+        repository="REPOSITORY"
+        style={styles[0]}
+        serviceSelection={serviceSelection}
+        handleRepositoryChange={noop}
+        handleServiceToggle={index => handleServiceToggleMocks[index]}
+        handleStyleChange={noop}
+      />,
+    );
+
+    services.forEach((service: Service, index: number) => {
+      expect(handleServiceToggleMocks[index]).toHaveBeenCalledTimes(0);
+      input
+        .find('input[type="checkbox"]')
+        .at(index)
+        .simulate('change');
+      expect(handleServiceToggleMocks[index]).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('calls handleStyleChange when respository changes', () => {
+    const handleStyleChangeMock = jest.fn();
+    const input = mount(
+      <Input
+        repository="REPOSITORY"
+        style={styles[0]}
+        serviceSelection={serviceSelection}
+        handleRepositoryChange={noop}
+        handleServiceToggle={noop}
+        handleStyleChange={handleStyleChangeMock}
+      />,
+    );
+
+    expect(handleStyleChangeMock).toHaveBeenCalledTimes(0);
+    input
+      .find('input[type="radio"][value="plastic"]')
+      .simulate('change', { target: { checked: true } });
+    expect(handleStyleChangeMock).toHaveBeenCalledTimes(1);
   });
 });
