ed1952eb3abcd78cbe383b1f6282997cce542011
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-13 12:25:33 +0100

    Added specs for config module

---
+++ src/config.spec.jsx
@@ -0,0 +1,27 @@
+import { formats } from './config';
+
+describe('formats', () => {
+  it('contains 9 formats', () => {
+    expect(formats.length).toBe(9);
+  });
+
+  it('contains 1 default format', () => {
+    expect(formats.filter(f => f.default).length).toBe(1);
+  });
+
+  formats.forEach(({ name, template }) => {
+    describe(`${name} template`, () => {
+      it('is a function', () => {
+        expect(template).toBeFunction();
+      });
+
+      it('returns a string', () => {
+        expect(template('TITLE', 'URL', 'IMAGE_URL')).toBeString();
+      });
+
+      it('returns a string that contains the URL or image URL', () => {
+        expect(template('TITLE', 'URL', 'IMAGE_URL')).toContain('URL');
+      });
+    });
+  });
+});
