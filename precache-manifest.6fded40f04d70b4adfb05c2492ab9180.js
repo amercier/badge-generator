ff1934ccd0436d7fa2c92c8c2613bb11d898eb5f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-13 08:26:11 +0000

    Deploy amercier/badge-generator to github.com/amercier/badge-generator.git:gh-pages

---
+++ precache-manifest.6fded40f04d70b4adfb05c2492ab9180.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0929d16e38adad1a978c",
+    "url": "/static/js/main.0929d16e.chunk.js"
+  },
+  {
+    "revision": "6ef49be1fff8c05da54a",
+    "url": "/static/js/1.6ef49be1.chunk.js"
+  },
+  {
+    "revision": "97b29feae41909582c4133571397bdf5",
+    "url": "/index.html"
+  }
+];
