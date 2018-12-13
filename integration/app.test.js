cffbbc2751cca43ddc6fbde113ace59788f8c78c
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-13 14:06:23 +0100

    Added keyboard typing delay

--- integration/app.test.js
+++ integration/app.test.js
@@ -28,7 +28,9 @@ test('Setting repository and enabling some services', async () => {
 
   // Set repository
   await page.focus('input[type="text"]');
-  await page.keyboard.type('amercier/badge-generator');
+  await page.keyboard.type('amercier/badge-generator', {
+    delay: 20,
+  });
 
   // Select services
   await page.click('label[title="Travis CI"]');
