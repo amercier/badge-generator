f95294eb0518c88f6b6084d0d012aae5e8b38fd6
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-13 19:26:50 +0100

    Wrapped test into a describe

--- integration/app.test.js
+++ integration/app.test.js
@@ -1,47 +1,49 @@
 const puppeteer = require('puppeteer');
 const { port } = require('../jest-puppeteer.config').server;
 
-let browser;
-let page;
+describe('App', () => {
+  let browser;
+  let page;
 
-beforeEach(async () => {
-  browser = await puppeteer.launch({
-    dumpio: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
-  page = await browser.newPage();
-  await page.emulate({
-    viewport: {
-      width: 1920,
-      height: 1080,
-    },
-    userAgent: 'Puppeteer',
-  });
-  await page.goto(`http://localhost:${port}/`);
-});
-
-afterEach(async () => {
-  await page.close();
-  await browser.close();
-});
-
-test('Setting repository and enabling some services', async () => {
-  await expect(page).toMatch('Github Badge Generator');
-
-  // Set repository
-  await page.focus('input[type="text"]');
-  await page.keyboard.type('amercier/badge-generator', {
-    delay: 20,
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
+      dumpio: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    page = await browser.newPage();
+    await page.emulate({
+      viewport: {
+        width: 1920,
+        height: 1080,
+      },
+      userAgent: 'Puppeteer',
+    });
+    await page.goto(`http://localhost:${port}/`);
   });
 
-  // Select services
-  await page.click('label[title="Travis CI"]');
-  await page.click('label[title="Codecov"]');
-  await page.click('label[title="David DM"]');
-  await page.click('label[title="David DM (dev)"]');
+  afterEach(async () => {
+    await page.close();
+    await browser.close();
+  });
 
-  const output = await page.evaluate(
-    () => document.querySelector('pre').innerText,
-  );
-  expect(output).toMatchSnapshot();
+  test('Setting repository and enabling some services', async () => {
+    await expect(page).toMatch('Github Badge Generator');
+
+    // Set repository
+    await page.focus('input[type="text"]');
+    await page.keyboard.type('amercier/badge-generator', {
+      delay: 20,
+    });
+
+    // Select services
+    await page.click('label[title="Travis CI"]');
+    await page.click('label[title="Codecov"]');
+    await page.click('label[title="David DM"]');
+    await page.click('label[title="David DM (dev)"]');
+
+    const output = await page.evaluate(
+      () => document.querySelector('pre').innerText,
+    );
+    expect(output).toMatchSnapshot();
+  });
 });
