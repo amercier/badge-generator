91e252a15d6c3313fb5ac018922d1aec17530540
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-14 07:09:58 +0100

    Added integration test using Puppeteer

---
+++ integration/app.test.js
@@ -0,0 +1,51 @@
+const puppeteer = require('puppeteer');
+const { port } = require('../jest-puppeteer.config').server;
+
+describe('App', () => {
+  let browser;
+  let page;
+
+  beforeEach(() => {
+    jest.setTimeout(30000);
+  });
+
+  test('Setting repository and enabling some services', async () => {
+    browser = await puppeteer.launch({
+      dumpio: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+
+    page = await browser.newPage();
+    await page.emulate({
+      viewport: {
+        width: 1920,
+        height: 1080,
+      },
+      userAgent: 'Puppeteer',
+    });
+    await page.goto(`http://localhost:${port}/`);
+
+    // Basic test
+    await expect(page).toMatch('Github Badge Generator');
+
+    // Set repository
+    await page.focus('input[type="text"]');
+    await page.keyboard.type('amercier/badge-generator', {
+      delay: 20,
+    });
+
+    // Select services
+    await page.click('label[title="Travis CI"]');
+    await page.click('label[title="Codecov"]');
+    await page.click('label[title="David DM"]');
+    await page.click('label[title="David DM (dev)"]');
+
+    const output = await page.evaluate(
+      () => document.querySelector('pre').innerText,
+    );
+    expect(output).toMatchSnapshot();
+
+    await page.close();
+    await browser.close();
+  });
+});
